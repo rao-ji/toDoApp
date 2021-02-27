88cbddf4c4be4daac855a0426c6ad55c052c74df
Abhishek Rao <arao8732@gmail.com>
2021-02-28 01:20:22 +0530

    first commit

---
+++ todoApp/server/task/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    TODO = 'todo'
+    DONE = 'done'
+
+    STATUS_CHOICES =(
+        (TODO, 'todo'),
+        (DONE, 'done')
+    )
+
+    description = models.CharField(max_length = 255)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default= TODO)
